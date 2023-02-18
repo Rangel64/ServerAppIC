235c3255f46fa778aa87702d502642fd70303d07
Rangel64 <rangersouza35@gmail.com>
2023-02-17 22:29:45 -0300

    teste

--- app_server.py
+++ app_server.py
@@ -10,15 +10,15 @@ app = Flask(__name__)
 
 
 def index():
-    global reponse
+    #global reponse
     
-    if(request.method == 'POST'):
-        request_data = request.data
-        request_data = json.loads(request_data.decode('utf-8'))
-        decodeString = request_data['image']
-        imageBytes = base64.b64decode((decodeString))
-        numeroDeAnimais = Estimador.result(imageBytes)
-        return jsonify({'response': str(numeroDeAnimais)})
+    #if(request.method == 'POST'):
+    #    request_data = request.data
+    #    request_data = json.loads(request_data.decode('utf-8'))
+    #    decodeString = request_data['image']
+    #    imageBytes = base64.b64decode((decodeString))
+    #    numeroDeAnimais = Estimador.result(imageBytes)
+    #    return jsonify({'response': str(numeroDeAnimais)})
     
     return "<h1>Hello World</h1>"  
         
