a61e67fa69a52f8d6bf0a4205e635e8e23ab8d49
Rangel64 <rangersouza35@gmail.com>
2023-02-17 22:17:02 -0300

    teste

--- app_server.py
+++ app_server.py
@@ -6,19 +6,21 @@ import Estimador
 
 app = Flask(__name__)
 
-@app.route('/', methods = ['GET','POST'])
+@app.route('/', methods = ['POST'])
 
 
 def index():
-        global reponse
+    global reponse
     
-    #if(request.method == 'POST'):
+    if(request.method == 'POST'):
         request_data = request.data
         request_data = json.loads(request_data.decode('utf-8'))
         decodeString = request_data['image']
         imageBytes = base64.b64decode((decodeString))
         numeroDeAnimais = Estimador.result(imageBytes)
         return jsonify({'response': str(numeroDeAnimais)})
+    
+    return "<h1>Hello World</h1>"  
         
 if(__name__ == "__main__"):
     app.run(host = 'localhost',debug=True)
